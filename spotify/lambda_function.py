409f9884051c8afd4fc9932f8f7568802d1a97f4
Samuel Schmidt <CSTSchmidt@gmail.com>
2019-05-02 21:37:53 -0700

    Add some logging for error cases and a bit more commenting

--- spotify/lambda_function.py
+++ spotify/lambda_function.py
@@ -3,18 +3,22 @@ import logging
 import boto3
 import base64
 
+# Public string identifying our Spotify app
 spotifyClientId = '5d01a10f2ad7422ebe8e97b71aa1cb11'
 
+# First, configure logging
 log = logging.getLogger()
+# Remove extra handlers added by AWS Lambda so we can log our own stuff
 if log.handlers:
     for handler in log.handlers:
         log.removeHandler(handler)
-logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
+# Log formatting!
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 
+# Invoked by AWS lambda
 def lambda_function(event, context):
-    log.info("function start")
-
+    log.info("lambda_function start")
 
     #debug logs for now
     log.info(event)
@@ -23,9 +27,9 @@ def lambda_function(event, context):
     log.info(context)
 
 
-
-
+    # Initialize Systems Manager client
     ssm = boto3.client('ssm')
+    # Retrieve spotify refresh token from Parameter Store
     parameter = ssm.get_parameter(Name='spotify.dash-button-scripts.refreshToken', WithDecryption=True)
     refreshToken = parameter['Parameter']['Value']
 
@@ -38,8 +42,6 @@ def lambda_function(event, context):
 
     r = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=headers)
 
-
-
     if r.status_code == requests.codes.ok:
 
         log.info(r.json())
@@ -47,30 +49,43 @@ def lambda_function(event, context):
         log.info(r.json()['item']['name'])
         log.info(r.json()['item']['uri'])
 
-    if inputClickType == 'SINGLE':
-        log.info('single click; save the song to Angela2019')
-        songUri = r.json()['item']['uri']
+        # If the user did a single click,
+        # save the current song to Angela2020 playlist
+        if inputClickType == 'SINGLE':
+            log.info('single click; save the song to Angela2019')
+            songUri = r.json()['item']['uri']
 
-        if songUri:
-            jsonPayload = {'uris': [songUri]}
+            if songUri:
+                jsonPayload = {'uris': [songUri]}
 
-            r = requests.post('https://api.spotify.com/v1/playlists/6c0212F4lnghWrAdPY4Aku/tracks', json=jsonPayload, headers=headers)
+                r = requests.post('https://api.spotify.com/v1/playlists/6c0212F4lnghWrAdPY4Aku/tracks', json=jsonPayload, headers=headers)
 
-            log.info(r.text);
+                log.info(r.text);
 
-    elif inputClickType == 'DOUBLE':
-        log.info('double click; save the album')
-        albumId = r.json()['item']['album']['id']
+        elif inputClickType == 'DOUBLE':
+            log.info('double click; save the album')
+            albumId = r.json()['item']['album']['id']
 
-        if albumId:
-            log.info(albumId)
-            jsonPayload = [albumId]
-            log.info(jsonPayload)
-            r = requests.put('https://api.spotify.com/v1/me/albums', json=jsonPayload, headers=headers)
+            if albumId:
+                log.info(albumId)
+                jsonPayload = [albumId]
+                log.info(jsonPayload)
+                r = requests.put('https://api.spotify.com/v1/me/albums', json=jsonPayload, headers=headers)
 
-    else:
-        log.error('invalid click type input!?')
+        else:
+            log.error('invalid click type input!?')
+            log.error(event)
+            log.error(context)
 
+    elif r.status_code == requests.codes.no_content: # if not a 200 OK response
+        # Everything is fine, just no currently playing song!
+        # We could get the last played song here, not yet implemented
+        log.info("The return code from Spotify.CurrentlyPlaying was " + str(r.status_code))
+        log.info("No action to take. Not a problem, carry on!")
+
+    else: # Some other code, oh no!
+        log.error("The return code from Spotify.CurrentlyPlaying was " + str(r.status_code))
+        log.error(r.json());
 
 
 def refreshTheToken(refreshToken):
